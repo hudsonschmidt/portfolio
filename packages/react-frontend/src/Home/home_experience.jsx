6297f19e0decbd38572e2487182edcaf3695cca8
hudsonschmidt <hudsonschmidt08@gmail.com>
2025-01-28 13:41:00 -0800

    fixed homepage buttons

--- packages/react-frontend/src/Home/home_experience.jsx
+++ packages/react-frontend/src/Home/home_experience.jsx
@@ -1,14 +1,17 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 function HomeExperience() {
   return (
     <div class="row">
-        <div id="experience" class="col-sm-6"></div>
-        <div id="column-2" class="col-sm-6">
-            <h3>Experience</h3>
-            <p>Apart from babysitting, I have spent most of my working life in restraunts. I also had the amazing opportunity to be an orientation leader for the freshmen at Cal Poly at the beginning of my second year.</p>
-            <a href="experience" class="btn" id="button">See More...</a>
-        </div>
+      <div id="experience" class="col-sm-6"></div>
+      <div id="column-2" class="col-sm-6">
+        <h3>Experience</h3>
+        <p>Apart from babysitting, I have spent most of my working life in restraunts. I also had the amazing opportunity to be an orientation leader for the freshmen at Cal Poly at the beginning of my second year.</p>
+        <Link className="btn" id="button" to="experience">
+          See More...
+        </Link>
+      </div>
     </div>
   );
 }
--- packages/react-frontend/src/Home/home_projects.jsx
+++ packages/react-frontend/src/Home/home_projects.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 function HomeProjects() {
     return (
@@ -6,11 +7,13 @@ function HomeProjects() {
             <div id="column-2" class="col-sm-6">
                 <h3>Projects</h3>
                 <p>I have been apart of multiple amazing opportunities that have made real-world differences. My current project is through the Empower Club at Cal Poly where we are designing a haptic headband to guide a blind marching band through their parade route.</p>
-                <a href="projects" class="btn" id="button">See More...</a>
+                <Link className="btn" id="button" to="projects">
+                    See More...
+                </Link>
             </div>
             <div id="projects" class="col-sm-6"></div>
         </div>
-      );
-    }
+    );
+}
 
 export default HomeProjects;
