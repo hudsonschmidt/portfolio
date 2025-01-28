109be5fbeabd20a8ef9367a613e97c5456fc2ef6
hudsonschmidt <hudsonschmidt08@gmail.com>
2025-01-28 11:58:28 -0800

    fixed base page

--- packages/react-frontend/vite.config.js
+++ packages/react-frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',  // This ensures the correct asset paths for GitHub Pages
+  base: '/',  // This ensures the correct asset paths for GitHub Pages
   build: {
     outDir: 'dist',
   }
