33e032356f38cf021ca4b58691962637c64c8604
hudsonschmidt <hudsonschmidt08@gmail.com>
2025-02-26 21:56:08 -0800

    fix

--- packages/react-frontend/src/Experience/exp_bd.jsx
+++ packages/react-frontend/src/Experience/exp_bd.jsx
@@ -8,7 +8,7 @@ function ExpBD() {
                 <h3>Blind Dog Restaurant</h3>
                 <p>Host, Food Runner, Expediter
                     <br></br>
-                    January 2023 - Current</p>
+                    January 2023 - Current (Seasonal)</p>
                 <p>When home from school, I work all front-of-house positions. This allows me to help out across the restaurant where needed throughout the night including running the kitchen by garnishing and timing every dish sent out.</p>
             </div>
         </div>
